7f201b4c46bee92d35f8c8dac3fbad6c1cbb73e5
Carlos Campderrós <carlos.campderros@eyeos.com>
2015-10-19 18:53:31 +0200

    inject env to Log4JsConfigReader (testability)

--- src/lib/tools/Log4JsConfigReader.js
+++ src/lib/tools/Log4JsConfigReader.js
@@ -1,19 +1,19 @@
 var fs = require('fs');
 
-function Log4JsConfigReader () {
-
+function Log4JsConfigReader (env) {
+	this.env = env || process.env;
 }
 
 Log4JsConfigReader.prototype.getDefaultConfig = function () {
 	return this._getLog4jsConfigFromEnvar() || {
 			'levels': {
-				'[all]': process.env.EYEOS_LOG_LEVEL || 'DEBUG'
+				'[all]': this.env.EYEOS_LOG_LEVEL || 'DEBUG'
 			}
 		};
 };
 
-	var filename = process.env.LOG4JS_CONFIG;
 Log4JsConfigReader.prototype._getLog4jsConfigFromEnvar = function () {
+	var filename = this.env.LOG4JS_CONFIG;
 	if (filename) {
 		return JSON.parse(fs.readFileSync(filename, "utf8"));
 	}
